36d126d4bc1bc7ee1d5fbfa77c2a679a283e0629
GabrielEdefors <gabriel_edefors@hotmail.com>
2019-06-30 22:39:21 +0200

    Version 2 with added excel sheet

--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 from tkinter import Tk, Label, Button, W, E, N, Entry, OptionMenu, StringVar
 from Moment_capacity import M_Rd
+import pandas as pd
 
 
 class CreateGUI:
@@ -11,6 +12,9 @@ class CreateGUI:
 
         self.exit = Button(parent, text="Exit", command=self.exit)
         self.exit.grid(row=7)
+
+        self.calculate = Button(parent, text="Calculate", command=self.calculate_moment_capacity)
+        self.calculate.grid(row=8)
 
         # Input - Effective height =====================================================================================
         self.labeld = Label(parent, text="d [m]")
@@ -33,30 +37,30 @@ class CreateGUI:
         self.entryfcd = Entry(parent)
         self.entryfcd.grid(row=3, column=1)
 
-        # Input - Steel stiffness ======================================================================================
-        self.labelEs = Label(parent, text="Es [GPa]")
-        self.labelEs.grid(row=4, sticky=E)
-
-        self.entryEs = Entry(parent)
-        self.entryEs.grid(row=4, column=1)
-
         # Input - Reinforcement type ===================================================================================
+
+        # Import reinforcement specs
+        self.reinforcement_table = pd.read_excel('InputData.xlsx', 'Reinforcement')
+
+        # Use the diamter as index
+        self.reinforcement_table.set_index('Diameter', inplace=True)
+
+        # Add a column with reinforcement name
+        self.reinforcement_table['name'] = '\u03D5' + self.reinforcement_table.index.astype(str) + 'B500B'
+
+        # Create label
         self.labeln = Label(parent, text="Number of bars ")
         self.labeln.grid(row=5, sticky=E)
 
+        # Create drop down menu
         self.entryn = Entry(parent)
         self.entryn.grid(row=5, column=1)
 
-        list_rebars = ['\u03D5' + '16B500B' ]
+        self.initial_value = StringVar(parent)
+        self.initial_value.set(self.reinforcement_table.iloc[3 ,-1])
 
-        initial_value = StringVar(parent)
-        initial_value.set(list_rebars[0])
-
-
-        self.popupMenu = OptionMenu(parent, initial_value, list_rebars)
+        self.popupMenu = OptionMenu(parent, self.initial_value, * self.reinforcement_table.name)
         self.popupMenu.grid(row=6, column=1)
-
-
 
     def exit(self):
         parent.destroy()
@@ -64,11 +68,28 @@ class CreateGUI:
     def calculate_moment_capacity(self):
 
         d = self.entryd.get()
-        # M_Rd(d, b, As, fcd, fyd, Es)
+        b = self.entryb.get()
+        fcd = self.entryfcd.get()
+        n = self.entryn.get()
+        reinforcment_type = self.initial_value.get()
+
+        # Calculate reinforce ment area
+        reinforcement_index = reinforcement_type
+        As = n * self.reinforcement_table.loc[reinforcement_type, 'Area']
+
+        print(As)
 
 
 
-parent = Tk()
-GUI_instance = CreateGUI(parent)
-parent.mainloop()
+
+        #M_Rd(d, b, As, fcd, fyd, Es)
+
+
+if __name__ == "__main__":
+    parent = Tk()
+    GUI_instance = CreateGUI(parent)
+    parent.mainloop()
+
+
+
 
